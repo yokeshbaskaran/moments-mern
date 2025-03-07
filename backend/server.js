ad8e2c7b5e730468893e078d28c0b102687031dd
Yokesh <yokeshbaskaran@gmail.com>
2025-03-07 13:22:00 +0530

    done

--- backend/server.js
+++ backend/server.js
@@ -39,10 +39,6 @@ app.use("/api", routes);
 
 // --------------------Deployment---------------
 
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
-});
-
 app.use(express.static(path.join(__dirname, "..", "frontend", "dist")));
 
 app.get("*", (req, res) => {
